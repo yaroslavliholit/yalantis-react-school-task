9f9a5309b8af0c9e7397acc3e44df6289507e39e
Yaroslav Liholit <yaroslavliholiti@gmail.com>
2020-01-02 16:55:20 +0200

    create footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FooterWrapper } from './FooterStyles';
+
+const Footer = () => (
+  <FooterWrapper>
+    <a href="/">
+      <img
+        src="https://raw.githubusercontent.com/github/explore/78df643247d429f6cc873026c0622819ad797942/topics/github/github.png" alt="github"
+        title="github repository"
+        width="30"
+        height="30" />
+    </a>
+  </FooterWrapper>
+);
+
+export default Footer;
