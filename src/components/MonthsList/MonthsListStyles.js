c2fd7b3b0faea9cff3c9142a332b8455dde2f3b1
Yaroslav Liholit <yaroslavliholiti@gmail.com>
2020-01-02 19:19:06 +0200

    fix style

--- src/components/MonthsList/MonthsListStyles.js
+++ src/components/MonthsList/MonthsListStyles.js
@@ -1,45 +1,32 @@
 import styled from 'styled-components';
 
 export const MonthslistWrapper = styled.ul`
-  padding: 0;
-  padding-bottom: 10px;
-  margin: 35px 0;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
+  margin: 35px 0;
+  padding: 0;
+  padding-bottom: 10px;
   border-bottom: 1px solid #e2e2e2;
 `;
 
 export const MonthslistItem = styled.li`
   position: relative;
-  padding: 0 10px;
+  margin: 3px;
+  width: 160px;
+  padding: 10px;
   text-align: center;
   list-style-type: none;
   font-family: 'Open Sans', sans-serif;
   font-size: 18px;
   font-weight: 700;
+  border: 1px solid #e2e2e2;
+  border-radius: 3px;
   color: ${({color}) => color};
   cursor: pointer;
+  transition: .5s border;
 
-  &:after {
-    position: absolute;
-    left: 0;
-    bottom: -12.5px;
-    content: '';
-    width: 100%;
-    height: 3px;
-    opacity: 0;
-    background-color: ${({color}) => color};
-    transition: .5s opacity;
-  }
-
-  &:hover:after {
-    opacity: 1;
-  }
-
-  @media (max-width: 1055px) {
-    &:after {
-      display: none;
-    }
+  &:hover {
+    border: 1px solid ${({color}) => color};
   }
 `;
