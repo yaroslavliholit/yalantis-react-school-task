3d9251537d6b49eb6678e757b7d71816fbcb7981
Yaroslav Liholit <yaroslavliholiti@gmail.com>
2020-01-03 10:14:35 +0200

    add try / catch block to getUsers request

--- src/services/http-services.js
+++ src/services/http-services.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 const API_URL = 'https://yalantis-react-school.herokuapp.com/api/task0/users';
 
 export const getUsers = async () => {
-  const responce = await axios.get(API_URL);
-  return responce;
+  try {
+    const responce = await axios.get(API_URL);
+    return responce;
+  } catch (error) {
+    console.error(error);
+  }
 };
